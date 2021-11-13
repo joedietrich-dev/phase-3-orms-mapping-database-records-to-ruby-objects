f13eb89981c0a59f52aad58f10792ea57918bcdc
joedietrich-dev <joe@joedietrich.net>
2021-11-13 16:28:04 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,23 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(name: row[1], album: row[2], id: row[0])
+  end
+
+  def self.all
+    res = DB[:conn].execute("SELECT * FROM songs")
+    res.map {|row| self.new_from_db row}
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1;
+    SQL
+
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db row}.first
+  end
 end
